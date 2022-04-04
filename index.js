100f6adefbe0de6a434654d24c25f3abba906032
Thatguyonfire240 <67000085+Thatguyonfire240@users.noreply.github.com>
2022-04-03 22:28:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react"
+
+const page = (
+    <div>
+        <h1>Dark Souls 1 Is The Best Game Ever Created!</h1>
+        <h3>Reasons why Dark Souls 1 is the best game created</h3>
+        <ol>
+            <li>It has a good challenge</li>
+            <li>It is a JRPG</li>
+            <li>It has many many hours of content</li>
+            <li>It takes great skill to beat the game</li>
+            <li>It does not hold your hand in anything</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
